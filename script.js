72a21a3869144628b382bbfd81cc35af47f5541e
Giprim <gi4real.gi@gmail.com>
2020-11-15 03:03:14 +0100

    feat: implement tabs navbar

---
+++ script.js
@@ -0,0 +1,29 @@
+// {
+//     viewer {
+//       login
+//       bio
+//       avatarUrl
+//       followers {
+//         totalCount
+//       }
+//       following {
+//         totalCount
+//       }
+//       location
+//       repositories(first: 20) {
+//         totalCount
+//         nodes {
+//           description
+//           name
+//           primaryLanguage {
+//             name
+//             color
+//           }
+//           forks {
+//             totalCount
+//           }
+//           updatedAt
+//         }
+//       }
+//     }
+//   }
