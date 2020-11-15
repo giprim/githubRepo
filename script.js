9b5130e23f0e97fa36feaa30c3b0f988764cfc31
Giprim <gi4real.gi@gmail.com>
2020-11-15 11:01:33 +0100

    feat: setup api queries

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
-// {
-//     viewer {
-//       login
-//       bio
-//       avatarUrl
-//       followers {
-//         totalCount
-//       }
-//       following {
-//         totalCount
-//       }
-//       location
-//       repositories(first: 20) {
-//         totalCount
-//         nodes {
-//           description
-//           name
-//           primaryLanguage {
-//             name
-//             color
-//           }
-//           forks {
-//             totalCount
-//           }
-//           updatedAt
-//         }
-//       }
-//     }
-//   }
+window.addEventListener("DOMContentLoaded", function () {
+  let query = `"query" : {
+    viewer,
+  };`;
+
+  queryFetch(`
+  query{
+    viewer {
+      login
+      bio
+      name
+      avatarUrl
+      followers {
+        totalCount
+      }
+      following {
+        totalCount
+      }
+      location
+      repositories(first: 20) {
+        totalCount
+        nodes {
+          description
+          name
+          primaryLanguage {
+            name
+            color
+          }
+          forks {
+            totalCount
+          }
+          updatedAt
+        }
+      }
+    }
+  }  
+  `).then((res) => console.log(res));
+
+  function queryFetch(query) {
+    return fetch("https://api.github.com/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "bearer a119abe8379b65ae41de47f5574cfcbe62397d6d",
+      },
+      body: JSON.stringify({
+        query: query,
+      }),
+    }).then((res) => res.json());
+  }
+});
