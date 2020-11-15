4145904f0a3154b490d8fa32816c53c199835dcb
Giprim <gi4real.gi@gmail.com>
2020-11-15 12:48:34 +0100

    feat: inject api data into the page

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.addEventListener("DOMContentLoaded", function () {
         totalCount
       }
       location
-      repositories(first: 20) {
+      repositories(first: 20, privacy: PUBLIC) {
         totalCount
         nodes {
           description
@@ -34,18 +34,111 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     }
   }  
-  `).then((res) => console.log(res));
+  `).then((res) => {
+    console.log(res.data);
+    console.log(res.data.viewer.bio);
+    let profilePicture = document.querySelectorAll(".profilePicture");
+
+    for (let i = 0; i < profilePicture.length; i++) {
+      profilePicture[i].setAttribute("src", res.data.viewer.avatarUrl);
+    }
+
+    let profileName = document.querySelector(".profileName");
+    profileName.textContent = res.data.viewer.name;
+
+    let profileNickName = document.querySelector(".profileNickName");
+    profileNickName.textContent = res.data.viewer.login;
+
+    let profileBio = document.querySelector(".bio");
+    profileBio.textContent = res.data.viewer.bio;
+
+    let repoTab = document.querySelector(".repoTab");
+    repoTab.textContent = res.data.viewer.repositories.totalCount;
+
+    let repositoriesBox = document.querySelector(".repositories");
+    for (let i = 0; i < res.data.viewer.repositories.nodes.length; i++) {
+      repositoriesBox.appendChild(
+        createRepoView(res.data.viewer.repositories.nodes[i])
+      );
+    }
+  });
 
   function queryFetch(query) {
     return fetch("https://api.github.com/graphql", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: "bearer a119abe8379b65ae41de47f5574cfcbe62397d6d",
+        Authorization: "bearer c0df7ce4f790934a35d74bc9aa9697cf7d90a6a8",
       },
       body: JSON.stringify({
         query: query,
       }),
     }).then((res) => res.json());
+  }
+
+  function createRepoView(repo) {
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    langColor = repo.primaryLanguage?.color;
+    let date = new Date(repo.updatedAt);
+
+    let flexBox = document.createElement("div");
+    let div = document.createElement("div");
+    let h3 = document.createElement("h3");
+    let a = document.createElement("a");
+    let flexBox2 = document.createElement("div");
+    let color = document.createElement("span");
+    let lang = document.createElement("span");
+    let lastUpdate = document.createElement("span");
+    let button = document.createElement("button");
+    let i = document.createElement("i");
+
+    a.setAttribute("href", "#");
+    a.textContent = repo.name;
+    h3.appendChild(a);
+
+    color.setAttribute("class", "color");
+    color.setAttribute("style", `background: ${langColor}`);
+
+    lang.setAttribute("class", "lang");
+    lang.textContent = repo.primaryLanguage?.name;
+
+    lastUpdate.setAttribute("class", "last-update");
+    lastUpdate.textContent = `Updated on ${
+      monthNames[date.getMonth()]
+    } ${date.getDay()}`;
+
+    flexBox2.setAttribute("class", "flex-box");
+    flexBox2.appendChild(color);
+    if (repo.primaryLanguage?.name) flexBox2.appendChild(lang);
+    flexBox2.appendChild(lastUpdate);
+
+    div.appendChild(h3);
+    div.appendChild(flexBox2);
+
+    button.setAttribute("class", "star btn-default");
+    i.setAttribute("class", "far fa-star");
+
+    button.appendChild(i);
+    button.append("Star");
+
+    flexBox.setAttribute("class", "flex-box repo border-bt");
+    flexBox.appendChild(div);
+    flexBox.appendChild(button);
+
+    return flexBox;
   }
 });
